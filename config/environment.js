945565afc3aa8d17ad36abfe3dcabb9bd71dd91b
Anthony Simone <asimone@elevatedthird.com>
2017-08-09 19:07:05 -0600

    Add ember-solr addon

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'morning-shelf',
     environment: environment,
+    solrBaseURL: 'http://YOUR-SOLR-HOST-HERE/',
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
